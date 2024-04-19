5b6b85cf374533ffe0783d2b30edc5009d55b9ab
Carmine Maggio <49706627+CarmineMM@users.noreply.github.com>
2024-04-19 14:03:36 -0400

    feat: debug options

--- Core/resources/views/exceptions/request.php
+++ Core/resources/views/exceptions/request.php
@@ -6,8 +6,27 @@
                 <td x-text="request.method"></td>
             </tr>
             <tr>
+                <th>URI</th>
+                <td x-text="'/'+request.uri"></td>
+            </tr>
+            <tr>
                 <th>URL</th>
                 <td x-text="request.url"></td>
+            </tr>
+            <tr>
+                <th>IP</th>
+                <td x-text="request.ip"></td>
+            </tr>
+            <tr>
+                <th>User Agent</th>
+                <td x-text="request.user_agent"></td>
+            </tr>
+            <tr>
+                <th>Is ajax</th>
+                <td x-text="request.isAjax ? 'true' : 'false'"></td>
+            </tr>
+            <tr>
+                <th></th>
             </tr>
         </tbody>
     </table>
