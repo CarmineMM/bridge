df0a2828079b9d2d63a1c5ccad235cf54f4793f3
Carmine Maggio <49706627+CarmineMM@users.noreply.github.com>
2024-05-17 05:28:04 -0400

    test

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@
  * @package Bridge
  * @version 1.0.0
  */
-require_once __DIR__ . '/../vendor/autoload.php';
-
+echo 'probar si funciona';
+// require_once __DIR__ . '/../vendor/autoload.php';
 
 if (phpversion() < 8.2) {
     die("Your PHP version must be 8.2 or higher to run <b>Minos Golding</b>. Current version: " . phpversion());
@@ -15,4 +15,4 @@ if (phpversion() < 8.2) {
 
 define('PUBLIC_PATH', dirname(__FILE__));
 
-\Core\Foundation\Application::run();
+// \Core\Foundation\Application::run();
