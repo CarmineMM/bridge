029f42893ffa0985705b41f12d96df511b83fa4d
Carmine Maggio <49706627+CarmineMM@users.noreply.github.com>
2024-05-17 04:47:58 -0400

    refactor: index.php

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bridge</title>
 </head>
+
 <body>
     <h1>Bridge</h1>
+    <?php
+    $get = shell_exec('ls');
+    echo "<pre>get: $get</pre>";
+    ?>
 </body>
+
 </html>
