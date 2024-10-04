3a7585831a53282d2541dd3e9f1873b36886f262
Carmine Maggio <carminemaggiom@gmail.com>
2024-10-03 21:26:43 -0400

    fix: framework name

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 if (phpversion() < 8.2) {
-    die("Your PHP version must be 8.2 or higher to run <b>Minos Golding</b>. Current version: " . phpversion());
+    die("Your PHP version must be 8.2 or higher to run <b>Bridge Framework</b>. Current version: " . phpversion());
 }
 
 define('PUBLIC_PATH', dirname(__FILE__));
