aeb7dcd0ab9d2bb2fe820e34d04eecec5c5c7d45
Carmine Maggio <carminemaggiom@gmail.com>
2024-10-08 23:54:33 -0400

    feat: prepare bridge component

--- Core/resources/js/bridge-wire-component.js
+++ Core/resources/js/bridge-wire-component.js
@@ -1,3 +1,11 @@
 document.addEventListener('alpine:init', () => {
-    
+    /**
+     * Componentes de Full Bridge
+     */
+    Alpine.data('full-bridge-component', (receivedData) => {
+        return {
+            loading: false,
+            data: {},
+        };
+    });
 });
